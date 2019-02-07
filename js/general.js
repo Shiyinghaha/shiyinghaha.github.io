8b5a60d83eb094709f4a51bfbb7bb416dfef78c4
shiyinghaha <lsy.sl327@gmail.com>
2019-02-06 23:36:31 -0500

    update

---
+++ js/general.js
@@ -0,0 +1,629 @@
+// //object em
+// var emperors=[
+// {
+//     eid : 1,
+//     ename  : "Augustus",
+//     deathClass: "dD",
+//     date:"August 19, 14 AD"
+// },
+// {
+//     eid : 2,
+//     ename  : "Tiberius",
+//     deathClass: "dD",
+//     date:"March 16, 37 AD"
+// },
+// {
+//     eid : 3,
+//     ename  : "Caligula",
+//     deathClass: "dA",
+//     date:"January 24, 41 AD"
+// },
+// {
+//     eid : 4,
+//     ename  : "Claudius",
+//     deathClass: "dD",
+//     date:"October 13, 54 AD"
+// },
+// {
+//     eid : 5,
+//     ename  : "Nero",
+//     deathClass: "dE",
+//     date:"June 9, 68 AD"
+// },
+// {
+//     eid : 6,
+//     ename  : "Galba",
+//     deathClass: "dA",
+//     date:"January 15, 69 AD"
+// },
+// {
+//     eid : 7,
+//     ename  : "Otho",
+//     deathClass: "dE",
+//     date:"April 16, 69 AD"
+// },
+// {
+//     eid : 8,
+//     ename  : "Vitellius",
+//     deathClass: "dD",
+//     date:"December 20, 69 AD"
+// },
+// {
+//     eid : 9,
+//     ename  : "Vespasian",
+//     deathClass: "dB",
+//     date:"June 24, 79 AD"
+// },
+// {
+//     eid : 10,
+//     ename  : "Titus",
+//     deathClass: "dB",
+//     date:"September 13, 81 AD"
+// },
+// {
+//     eid : 11,
+//     ename  : "Domitian",
+//     deathClass: "dA",
+//     date:"September 18, 96 AD"
+// },
+// {
+//     eid : 12,
+//     ename  : "Nerva",
+//     deathClass: "dB",
+//     date:"January 27, 98 AD"
+// },
+// {
+//     eid : 13,
+//     ename  : "Trajan",
+//     deathClass: "dB",
+//     date:"August 7, 117 AD"
+// },
+// {
+//     eid : 14,
+//     ename  : "Hadrian",
+//     deathClass: "dB",
+//     date:"July 10, 138 AD"
+// },
+// {
+//     eid : 15,
+//     ename  : "Antoninus Pius",
+//     deathClass: "dB",
+//     date:"March 7, 161 AD"
+// },
+// {
+//     eid : 16,
+//     ename  : "Marcus Aurelius",
+//     deathClass: "dB",
+//     date:"March 17, 180 AD"
+// },
+// {
+//     eid : 17,
+//     ename  : "Lucius Verus",
+//     deathClass: "dB",
+//     date:"March 169 AD"
+// },
+// {
+//     eid : 18,
+//     ename  : "Commodus",
+//     deathClass: "dA",
+//     date:"December 31, 192 AD"
+// },
+// {
+//     eid : 19,
+//     ename  : "Pertinax",
+//     deathClass: "dA",
+//     date:"March 28, 193 AD"
+// },
+// {
+//     eid : 20,
+//     ename  : "Didius Julianus",
+//     deathClass: "dA",
+//     date:"June 1, 193 AD"
+// },
+// {
+//     eid : 21,
+//     ename  : "Septimius Severus",
+//     deathClass: "dB",
+//     date:"February 4, 211 AD"
+// },
+// {
+//     eid : 22,
+//     ename  : "Caracalla",
+//     deathClass: "dA",
+//     date:"April 8, 217 AD"
+// },
+// {
+//     eid : 23,
+//     ename  : "Geta",
+//     deathClass: "dA",
+//     date:"December 19, 211 AD"
+// },
+// {
+//     eid : 24,
+//     ename  : "Macrinus",
+//     deathClass: "dF",
+//     date:"June 8, 218 AD"
+// },
+// {
+//     eid : 25,
+//     ename  : "Diadumenian",
+//     deathClass: "dF",
+//     date:"June 8, 218 AD"
+// },
+// {
+//     eid : 26,
+//     ename  : "Elagabalus",
+//     deathClass: "dA",
+//     date:"March 11, 222 AD"
+// },
+// {
+//     eid : 27,
+//     ename  : "Serverus Alexander",
+//     deathClass: "dA",
+//     date:"March 18, 235 AD"
+// },
+// {
+//     eid : 28,
+//     ename  : "Maximinus I",
+//     deathClass: "dA",
+//     date:"June 238 AD"
+// },
+// {
+//     eid : 29,
+//     ename  : "Gordian I",
+//     deathClass: "dE",
+//     date:"April 238 AD"
+// },
+// {
+//     eid : 30,
+//     ename  : "Gordian II",
+//     deathClass: "dC",
+//     date:"April 238 AD"
+// },
+// {
+//     eid : 31,
+//     ename  : "Pupienus",
+//     deathClass: "dA",
+//     date:"July 29, 238 AD"
+// },
+// {
+//     eid : 32,
+//     ename  : "Balbinus",
+//     deathClass: "dA",
+//     date:"July 29, 238 AD"
+// },
+// {
+//     eid : 33,
+//     ename  : "Gordian III",
+//     deathClass: "dD",
+//     date:"February 11, 244 AD"
+// },
+// {
+//     eid : 34,
+//     ename  : "Philip I",
+//     deathClass: "dC",
+//     date:"September/October 249 AD"
+// },
+// {
+//     eid : 35,
+//     ename  : "Trajan Decius",
+//     deathClass: "dC",
+//     date:"June 251 AD"
+// },
+// {
+//     eid : 36,
+//     ename  : "Herennius Etruscus",
+//     deathClass: "dC",
+//     date:"June 251 AD"
+// },
+// {
+//     eid : 37,
+//     ename  : "Hostilian",
+//     deathClass: "dB",
+//     date:"September/October 251 AD"
+// },
+// {
+//     eid : 38,
+//     ename  : "Trebonianus",
+//     deathClass: "dA",
+//     date:"August 253 AD"
+// },
+// {
+//     eid : 39,
+//     ename  : "Aemilian",
+//     deathClass: "dA",
+//     date:"September/October 253 AD"
+// },
+// {
+//     eid : 40,
+//     ename  : "Valerian",
+//     deathClass: "dG",
+//     date:"After 260 AD"
+// },
+// {
+//     eid : 41,
+//     ename  : "Gallienus",
+//     deathClass: "dA",
+//     date:"September 268 AD"
+// },
+// {
+//     eid : 42,
+//     ename  : "Claudius Gothicus",
+//     deathClass: "dB",
+//     date:"January 270 AD"
+// },
+// {
+//     eid : 43,
+//     ename  : "Quintillus",
+//     deathClass: "dH",
+//     date:"270 AD"
+// },
+// {
+//     eid : 44,
+//     ename  : "Aurelian",
+//     deathClass: "dA",
+//     date:"September 275 AD"
+// },
+// {
+//     eid : 45,
+//     ename  : "Tacitus",
+//     deathClass: "dD",
+//     date:"June 276 AD"
+// },
+// {
+//     eid : 46,
+//     ename  : "Florian",
+//     deathClass: "dA",
+//     date:"September 276 AD"
+// },
+// {
+//     eid : 47,
+//     ename  : "Probus",
+//     deathClass: "dA",
+//     date:"September/October 282 AD"
+// },
+// {
+//     eid : 48,
+//     ename  : "Carus",
+//     deathClass: "dD",
+//     date: "July/August 283 AD"
+// },
+// {
+//     eid : 49,
+//     ename  : "Numerian",
+//     deathClass: "dD",
+//     date:"284 AD"
+// },
+// {
+//     eid : 50,
+//     ename  : "Carinus",
+//     deathClass: "dC",
+//     date:"285 AD"
+// },
+// {
+//     eid : 51,
+//     ename  : "Diocletian",
+//     deathClass: "dB",
+//     date:"3 December 311 AD"
+// },
+// {
+//     eid : 52,
+//     ename  : "Maximian",
+//     deathClass: "dE",
+//     date:"310 AD"
+// },
+// {
+//     eid : 53,
+//     ename  : "Constantius I",
+//     deathClass: "dB",
+//     date:"306 AD"
+// },
+// {
+//     eid : 54,
+//     ename  : "Galerius",
+//     deathClass: "dB",
+//     date:"311 AD"
+// },
+// {
+//     eid : 55,
+//     ename  : "Serverus II",
+//     deathClass: "dA",
+//     date:"September 16, 307 AD"
+// },
+// {
+//     eid : 56,
+//     ename  : "Constantine the Great",
+//     deathClass: "dB",
+//     date:"May 22, 337 AD"
+// },
+// {
+//     eid : 57,
+//     ename  : "Maxentius",
+//     deathClass: "dC",
+//     date:"October 28, 312 AD"
+// },
+// {
+//     eid : 58,
+//     ename  : "Maximinus II",
+//     deathClass: "dE",
+//     date:"July/August 313 AD"
+// },
+// {
+//     eid : 59,
+//     ename  : "Lucinius",
+//     deathClass: "dF",
+//     date:"325 AD"
+// },
+// {
+//     eid : 60,
+//     ename  : "Constantine II",
+//     deathClass: "dC",
+//     date:"340 AD"
+// },
+// {
+//     eid : 61,
+//     ename  : "Constantius II",
+//     deathClass: "dB",
+//     date:"361 AD"
+// },
+// {
+//     eid : 62,
+//     ename  : "Constans I",
+//     deathClass: "dA",
+//     date:"350 AD"
+// },
+// {
+//     eid : 63,
+//     ename  : "Vetranio",
+//     deathClass: "dB",
+//     date:"356 AD"
+// },
+// {
+//     eid : 64,
+//     ename  : "Julian",
+//     deathClass: "dC",
+//     date:"June 26, 363 AD"
+// },
+// {
+//     eid : 65,
+//     ename  : "Jovian",
+//     deathClass: "dB",
+//     date:"February 17, 364 AD"
+// },
+// {
+//     eid : 66,
+//     ename  : "Valentinian I",
+//     deathClass: "dB",
+//     date:"November 17, 375 AD"
+// },
+// {
+//     eid : 67,
+//     ename  : "Valens",
+//     deathClass: "dC",
+//     date:"August 9, 378 AD"
+// },
+// {
+//     eid : 68,
+//     ename  : "Gratian",
+//     deathClass: "dA",
+//     date:"August 25, 383 AD"
+// },
+// {
+//     eid : 69,
+//     ename  : "Valentinian II",
+//     deathClass: "dD",
+//     date:"May 15, 392 AD"
+// },
+// {
+//     eid : 70,
+//     ename  : "Theodosius",
+//     deathClass: "dB",
+//     date:"January 17, 395 AD"
+// },
+// ];
+
+// var notSli=false;
+// var all=true;
+// var act=[true,true,true,true,true];
+// function death(s){
+// 	var val =document.getElementById('exploreB');
+// 	// for (i=0;i<act.length;i++){
+// 	// 	if(act[i]==false){
+// 	// 		all=false;
+// 	// 		break;
+// 	// 	}
+// 	// }
+// 	if(!act[s]||all){
+// 		all=false;
+// 		val.value="Explore This";
+// 		// $('#exploreB').value="Explore This";
+// 		if(s==0){
+// 			$(".dA").css( "opacity", 1.);
+// 			act[0]=true;
+// 			act[1]=false;
+// 			act[2]=false;
+// 			act[3]=false;
+// 			act[4]=false;
+// 			$(".dB").css( "opacity", .5);
+// 			$(".dC").css( "opacity", .5);
+// 			$(".dD").css( "opacity", .5);
+// 			$(".dE").css( "opacity", .5);
+// 			$(".dF").css( "opacity", .5);
+// 			$(".dG").css( "opacity", .5);
+// 			$(".dH").css( "opacity", .5);
+// 		}else if (s==1){
+// 			$(".dB").css( "opacity", 1.);
+// 			$(".dA").css( "opacity", .5);
+// 			$(".dC").css( "opacity", .5);
+// 			$(".dD").css( "opacity", .5);
+// 			$(".dE").css( "opacity", .5);
+// 			$(".dF").css( "opacity", .5);
+// 			$(".dG").css( "opacity", .5);
+// 			$(".dH").css( "opacity", .5);
+// 			act[0]=false;
+// 			act[1]=true;
+// 			act[2]=false;
+// 			act[3]=false;
+// 			act[4]=false;
+// 		}else if (s==2){
+// 			$(".dC").css( "opacity", 1.);
+// 			$(".dB").css( "opacity", .5);
+// 			$(".dA").css( "opacity", .5);
+// 			$(".dD").css( "opacity", .5);
+// 			$(".dE").css( "opacity", .5);
+// 			$(".dF").css( "opacity", .5);
+// 			$(".dG").css( "opacity", .5);
+// 			$(".dH").css( "opacity", .5);
+// 			act[0]=false;
+// 			act[1]=false;
+// 			act[2]=true;
+// 			act[3]=false;
+// 			act[4]=false;
+// 		}else if (s==3){
+// 			$(".dD").css( "opacity", 1.);
+// 			$(".dB").css( "opacity", .5);
+// 			$(".dC").css( "opacity", .5);
+// 			$(".dA").css( "opacity", .5);
+// 			$(".dE").css( "opacity", .5);
+// 			$(".dF").css( "opacity", .5);
+// 			$(".dG").css( "opacity", .5);
+// 			$(".dH").css( "opacity", .5);
+// 			act[0]=false;
+// 			act[1]=false;
+// 			act[2]=false;
+// 			act[3]=true;
+// 			act[4]=false;
+// 		}else if (s==4){
+// 			$(".dE").css( "opacity", 1.);
+// 			$(".dF").css( "opacity", 1.);
+// 			$(".dG").css( "opacity", 1.);
+// 			$(".dH").css( "opacity", 1.);
+// 			$(".dA").css( "opacity", .5);
+// 			$(".dB").css( "opacity", .5);
+// 			$(".dC").css( "opacity", .5);
+// 			$(".dD").css( "opacity", .5);	
+// 			act[0]=false;
+// 			act[1]=false;
+// 			act[2]=false;
+// 			act[3]=false;
+// 			act[4]=true;		
+// 		}
+// 	}else{
+// 		all=true;
+// 		val.value="Explore All";
+// 		$(".dA").css( "opacity", 1);
+// 		$(".dB").css( "opacity", 1);
+// 		$(".dC").css( "opacity", 1);
+// 		$(".dD").css( "opacity", 1);
+// 		$(".dE").css( "opacity", 1);
+// 		$(".dF").css( "opacity", 1);
+// 		$(".dG").css( "opacity", 1);
+// 		$(".dH").css( "opacity", 1);
+// 		for(i=0;i<act.length;i++){
+// 			act[i]=true;
+// 		}
+// 	}
+	
+// 	}
+
+
+
+// function disappear(){
+// 	notSli=true;
+// //	document.getElementById("exploreB").value="Explore This";
+// 	//$(".pBlock").css("background-color","#000000");
+// 	// $("#fMenu").fadeOut(1000,function(){
+// 	// });
+// 	$("#fMenu").animate({
+//     height:"0"
+// },400,'easeInSine',function(){
+// 		var start = new Event('Event');
+//     start.initEvent('anim', true, true);
+//     document.dispatchEvent(start);
+// });
+// 	$("#se-nav").show();
+// 	$(".nav-hd").animate({
+//     height:"180px"
+// },400,'easeOutSine');
+
+// }
+
+// function slidIn(count){
+// 	if(!notSli){
+// 	var idd='#s'+count.toString();
+// 	if(! $(idd).onmouseover){
+// 	$(idd).animate({
+//      height:"40%"
+//  },400,'swing');}
+// }
+// }
+
+// function slidOut(count){
+// 	if(!notSli){
+// 	var idd='#s'+count.toString();
+// 	if(! $(idd).onmouseover){
+// 		$(idd).animate({
+//      height:"0"
+//  },400,'swing');}
+// 	}
+// }
+
+
+// document.addEventListener('anim',function(e){
+// var d;
+// var list='<ul class="list-group pre-scrollable">';
+// var typ;
+// var de=['dA','dB','dC','dD','dE','dF','dG','dH'];
+// //alert(act[0]);
+// //list display emperor name and maybe death time
+// if(!all){
+// 	for(i=0;i<act.length;i++){
+//     if(act[i]){
+//         if(i<4){
+//         	if(i==0){
+//         		typ='Assasinated';
+//         	}else if (i==1){
+//         		typ='Natural Cause';
+//         	}else if (i==2){
+//         		typ='During the Battle';
+//         	}else if (i==3){
+//         		typ='Probably Assasinated';
+//         	}
+//             for(j=0;j<emperors.length;j++){
+//                 if(emperors[j].deathClass==de[i]){
+//                     list=list+'<li class="list-group-item">'+emperors[j].ename+'<span class="spanList">'+emperors[j].date+'</span></li>';
+//                 }
+//             }
+//         }else{
+//         	typ='Other Reasons';
+//         	for(j=0;j<emperors.length;j++){
+//             if(emperors[j].deathClass==de[4] || (emperors[j].deathClass==de[5]) || (emperors[j].deathClass==de[6]) || (emperors[j].deathClass==de[7])){
+//                     list=list+'<li class="list-group-item">'+emperors[j].ename+'<span class="spanList">'+emperors[j].date+'</span></li>';
+//                 }
+//             }
+//         }
+//     }
+// }
+//  var temp='<div class="panel-heading text-center">Date of Death<br><span class="spanPHead">'+typ+'</span></div>';
+// }else{
+// 	for(j=0;j<emperors.length;j++){
+// 		list=list+'<li class="list-group-item">'+emperors[j].ename+'<span class="spanList">'+emperors[j].date+'</span></li>';
+// 	}
+// 	var temp='<div class="panel-heading text-center">Date of Death<br><span class="spanPHead">All Emperors</span></div>';
+// }
+//  	document.getElementById("mainUl").innerHTML =temp+list+'</ul>';
+//  	// var ht;
+//  	// if($( window ).width()>768){
+//  	// 	ht="550px";
+//  	// }else{
+//  	// 	ht="800px";
+//  	// }
+
+ 	
+
+ 	
+
+// 	$(".mainA").show();
+// });
+
+
